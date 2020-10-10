4db225b55690385e08a876118b58f97e9f40c419
Egor Astakhov <noise718@gmail.com>
2020-10-10 08:00:36 +0300

    Initial Commit

---
+++ The Light Raibow/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  The Light Raibow
+//
+//  Created by Егор Астахов on 10.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
