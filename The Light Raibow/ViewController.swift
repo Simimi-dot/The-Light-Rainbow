e569dd853b1eb0567b398919c71d5ae5d3541bb1
Egor Astakhov <noise718@gmail.com>
2020-10-10 15:42:37 +0300

    The Light Rainbow with FlashLight.

--- The Light Raibow/ViewController.swift
+++ The Light Raibow/ViewController.swift
@@ -6,14 +6,80 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    let colorMax = 6
+    var colorMin = 0
+    
+    override var prefersStatusBarHidden: Bool{
+        return true
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        UpdateUI()
+        toggleTorch(on: true)
     }
+    
+    fileprivate func UpdateUI() {
+        switch colorMin {
+        case 1:
+            view.backgroundColor = .orange
+        case 2:
+            view.backgroundColor = .yellow
+        case 3:
+            view.backgroundColor = .green
+        case 4:
+            view.backgroundColor = .cyan
+        case 5:
+            view.backgroundColor = .blue
+        case 6:
+            view.backgroundColor = .purple
+        default:
+            view.backgroundColor = .red
+        }
+    }
+    
+    @IBAction func pushButton() {
+        colorMin += 1
+        if colorMax < colorMin {
+            colorMin = 0
+        }
+        UpdateUI()
+        toggleTorch(on: true)
+        
+    }
+    
+    
+    func toggleTorch(on: Bool) {
+        guard let device = AVCaptureDevice.default(for: .video) else { return }
 
+        if device.hasTorch {
+            do {
+                try device.lockForConfiguration()
 
+                if on == true {
+                    device.torchMode = .on
+                } else {
+                    device.torchMode = .off
+                }
+
+                device.unlockForConfiguration()
+            } catch {
+                print("Torch could not be used")
+            }
+        } else {
+            print("Torch is not available")
+        }
+    }
+    
+   
+    
 }
+    
+
+
+    
 
