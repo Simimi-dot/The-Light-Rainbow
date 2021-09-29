274516583660744be81b95a4454556157c33faba
Egor Astakhov <noise718@gmail.com>
2021-09-29 15:46:30 +0300

    Added rainbow colors and their switching

--- The Light Rainbow/ViewController.swift
+++ The Light Rainbow/ViewController.swift
@@ -8,12 +8,45 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let maxColors = 7
+    var currentNumberOfColor = 0
 
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
     }
 
 
+    @IBAction func colorButton() {
+        
+        currentNumberOfColor += 1
+        
+        switch currentNumberOfColor {
+        case 1:
+            view.backgroundColor = .orange
+        case 2:
+            view.backgroundColor = .yellow
+        case 3:
+            view.backgroundColor = .green
+        case 4:
+            view.backgroundColor = .cyan
+        case 5:
+            view.backgroundColor = .blue
+        case 6:
+            view.backgroundColor = .purple
+        default:
+            view.backgroundColor = .red
+        }
+        
+        if currentNumberOfColor == 7 {
+            currentNumberOfColor = 0
+        }
+        
+    }
 }
 
