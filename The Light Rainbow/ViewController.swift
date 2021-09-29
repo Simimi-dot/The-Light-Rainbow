8018a4042b1524ca249eb06cb080e1e4cdd3d25e
Egor Astakhov <noise718@gmail.com>
2021-09-29 15:31:28 +0300

    Initial Commit

---
+++ The Light Rainbow/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  The Light Rainbow
+//
+//  Created by Егор Астахов on 29.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
